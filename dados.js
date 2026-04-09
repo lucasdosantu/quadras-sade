83b01e3d8373cc83de9284e180bdb4a629e4eb40
lucasdosantu <106567042+lucasdosantu@users.noreply.github.com>
2026-04-08 21:36:57 -0300

    commit inicial
    adiciona o app index.html e a tabela de dados.js

---
+++ dados.js
@@ -0,0 +1,9 @@
+const baseDeDados = [
+    {
+        "bairro": "Centro",
+        "quadra": "10",
+        "lat": -15.94379,
+        "lng": -48.266619,
+        "obs": "Quadra do Restaurante Gula's"
+    }
+];
