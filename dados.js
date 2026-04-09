9d162ca7f5145c943c05cd6a7f206ea82f1840af
lucasdosantu <106567042+lucasdosantu@users.noreply.github.com>
2026-04-08 21:55:42 -0300

    Atualizar o dados.js
    add quadra 54 centro

--- dados.js
+++ dados.js
@@ -5,5 +5,12 @@ const baseDeDados = [
         "lat": -15.94379,
         "lng": -48.266619,
         "obs": "Quadra do Restaurante Gula's"
+    },
+    {
+        "bairro": "Centro",
+        "quadra": "54",
+        "lat": -15.943733,
+        "lng": -48.264815,
+        "obs": "Quadra da agência da Caixa"
     }
 ];
