e189c16005f1df3f6bb0c00463f240577bda0ed7
lucasdosantu <106567042+lucasdosantu@users.noreply.github.com>
2026-04-08 21:49:48 -0300

    Atualizar o dados.js
    atualização quadra 61 centro

--- dados.js
+++ dados.js
@@ -1,7 +1,7 @@
 const baseDeDados = [
     {
         "bairro": "Centro",
-        "quadra": "10",
+        "quadra": "61",
         "lat": -15.94379,
         "lng": -48.266619,
         "obs": "Quadra do Restaurante Gula's"
