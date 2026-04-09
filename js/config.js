ec535674a08f39aead30e613b5b25feb4bf02f0d
lucasdosantu <106567042+lucasdosantu@users.noreply.github.com>
2026-04-09 10:21:49 -0300

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,10 @@
+const layerRua = L.tileLayer('https://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', { maxZoom: 20, subdomains:['mt0','mt1','mt2','mt3'] });
+const layerSatelite = L.tileLayer('https://{s}.google.com/vt/lyrs=y&x={x}&y={y}&z={z}', { maxZoom: 20, subdomains:['mt0','mt1','mt2','mt3'] });
+const map = L.map('map', { layers: [layerRua] }).setView([-15.943, -48.265], 15);
+
+let marcadorAtual, marcadorVoce, modoSugestao = false, coordsUsuario = null;
+
+function toggleSatelite() {
+    if (map.hasLayer(layerRua)) { map.removeLayer(layerRua); map.addLayer(layerSatelite); }
+    else { map.removeLayer(layerSatelite); map.addLayer(layerRua); }
+}
