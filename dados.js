20ad99ced933f70bdf205cbe5bac93c05e0b9bbc
lucasdosantu <106567042+lucasdosantu@users.noreply.github.com>
2026-04-08 23:17:18 -0300

    Atualizar o dados.js
    add presídio/campo gramado
    add rodoviária/feira coberta
    add quadra 47 centro
    add quadra 68 centro

--- dados.js
+++ dados.js
@@ -12,5 +12,33 @@ const baseDeDados = [
         "lat": -15.943733,
         "lng": -48.264815,
         "obs": "Quadra da agência da Caixa"
+    },
+    {
+        "bairro": "Centro",
+        "quadra": "68",
+        "lat": -15.943977,
+        "lng": -48.268284,
+        "obs": "Quadra frente ao campo gramado"
+    },
+    {
+        "bairro": "Centro",
+        "quadra": "47",
+        "lat": -15.943725,
+        "lng": -48.264858,
+        "obs": "Quadra em frente a rodoviária"
+    },
+    {
+        "bairro": "Centro",
+        "quadra": "Rodoviária/Feira coberta",
+        "lat": -15.944483,
+        "lng": -48.264747,
+        "obs": ""
+    },
+    {
+        "bairro": "Centro",
+        "quadra": "Presídio/Campo gramado",
+        "lat": -15.944751,
+        "lng": -48.268436,
+        "obs": ""
     }
 ];
