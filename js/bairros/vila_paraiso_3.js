716189ed9d392784612fe1c28f353e8b1747f221
lucasdosantu <106567042+lucasdosantu@users.noreply.github.com>
2026-05-02 15:07:27 -0300

    Criar vila_paraiso_3.js

---
+++ js/bairros/vila_paraiso_3.js
@@ -0,0 +1,26 @@
+baseDeDados.push(...[
+	{
+		"bairro": "Vila Paraíso 3",
+		"quadra": "B",
+		"cep": "72900773",
+		"lat": -15.948402,
+		"lng": -48.255816,
+		"obs": ""
+	},
+	{
+		"bairro": "Vila Paraíso 3",
+		"quadra": "C",
+		"cep": "72900776",
+		"lat": -15.948975,
+		"lng": -48.255823,
+		"obs": ""
+	},
+	{
+		"bairro": "Vila Paraíso 3",
+		"quadra": "D",
+		"cep": "72900779",
+		"lat": -15.948027,
+		"lng": -48.255255,
+		"obs": ""
+	}
+	]);
