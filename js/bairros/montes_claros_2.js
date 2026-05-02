a217a33e9dd7b756908379ff158a1c302bd6dabe
lucasdosantu <106567042+lucasdosantu@users.noreply.github.com>
2026-05-02 15:06:03 -0300

    Criar montes_claros_2.js

---
+++ js/bairros/montes_claros_2.js
@@ -0,0 +1,242 @@
+baseDeDados.push(...[
+	{
+		"bairro": "Montes Claros 2",
+		"quadra": "1",
+		"cep": "72906770",
+		"lat": -15.955607,
+		"lng": -48.260748,
+		"obs": "Avenida das Palmeiras"
+	},
+	{
+		"bairro": "Montes Claros 2",
+		"quadra": "2",
+		"cep": "72906716",
+		"lat": -15.955148,
+		"lng": -48.260599,
+		"obs": ""
+	},
+	{
+		"bairro": "Montes Claros 2",
+		"quadra": "3",
+		"cep": "72906720",
+		"lat": -15.954641,
+		"lng": -48.260221,
+		"obs": ""
+	},
+	{
+		"bairro": "Montes Claros 2",
+		"quadra": "4",
+		"cep": "72906776",
+		"lat": -15.954832,
+		"lng": -48.258433,
+		"obs": ""
+	},
+	{
+		"bairro": "Montes Claros 2",
+		"quadra": "5",
+		"cep": "72906778",
+		"lat": -15.955204,
+		"lng": -48.258824,
+		"obs": ""
+	},
+	{
+		"bairro": "Montes Claros 2",
+		"quadra": "6",
+		"cep": "72906780",
+		"lat": -15.955593,
+		"lng": -48.259162,
+		"obs": ""
+	},
+	{
+		"bairro": "Montes Claros 2",
+		"quadra": "7",
+		"cep": "72906782",
+		"lat": -15.955998,
+		"lng": -48.259523,
+		"obs": ""
+	},
+	{
+		"bairro": "Montes Claros 2",
+		"quadra": "8",
+		"cep": "72906784",
+		"lat": -15.956356,
+		"lng": -48.259834,
+		"obs": "Avenida das Palmeiras"
+	},
+	{
+		"bairro": "Montes Claros 2",
+		"quadra": "9",
+		"cep": "72906810",
+		"lat": -15.956718,
+		"lng": -48.26057,
+		"obs": "Avenida das Palmeiras"
+	},
+	{
+		"bairro": "Montes Claros 2",
+		"quadra": "10",
+		"cep": "72906812",
+		"lat": -15.957293,
+		"lng": -48.260841,
+		"obs": ""
+	},
+	{
+		"bairro": "Montes Claros 2",
+		"quadra": "11",
+		"cep": "72906822",
+		"lat": -15.957705,
+		"lng": -48.261093,
+		"obs": ""
+	},
+	{
+		"bairro": "Montes Claros 2",
+		"quadra": "12",
+		"cep": "72906824",
+		"lat": -15.958209,
+		"lng": -48.261374,
+		"obs": ""
+	},
+	{
+		"bairro": "Montes Claros 2",
+		"quadra": "13",
+		"cep": "72906826",
+		"lat": -15.959133,
+		"lng": -48.260378,
+		"obs": ""
+	},
+	{
+		"bairro": "Montes Claros 2",
+		"quadra": "14",
+		"cep": "72906820",
+		"lat": -15.958715,
+		"lng": -48.260095,
+		"obs": ""
+	},
+	{
+		"bairro": "Montes Claros 2",
+		"quadra": "15",
+		"cep": "72906814",
+		"lat": -15.958302,
+		"lng": -48.25967,
+		"obs": ""
+	},
+	{
+		"bairro": "Montes Claros 2",
+		"quadra": "16",
+		"cep": "72906808",
+		"lat": -15.957847,
+		"lng": -48.259263,
+		"obs": "Avenida das Palmeiras"
+	},
+	{
+		"bairro": "Montes Claros 2",
+		"quadra": "17",
+		"cep": "72906786",
+		"lat": -15.957442,
+		"lng": -48.258766,
+		"obs": "Avenida das palmeiras"
+	},
+	{
+		"bairro": "Montes Claros 2",
+		"quadra": "18",
+		"cep": "72906788",
+		"lat": -15.95678,
+		"lng": -48.258154,
+		"obs": "CEPI"
+	},
+	{
+		"bairro": "Montes Claros 2",
+		"quadra": "19",
+		"cep": "72906790",
+		"lat": -15.956206,
+		"lng": -48.25772,
+		"obs": ""
+	},
+	{
+		"bairro": "Montes Claros 2",
+		"quadra": "20",
+		"cep": "72906792",
+		"lat": -15.955787,
+		"lng": -48.257282,
+		"obs": ""
+	},
+	{
+		"bairro": "Montes Claros 2",
+		"quadra": "21",
+		"cep": "72906794",
+		"lat": -15.956921,
+		"lng": -48.256581,
+		"obs": ""
+	},
+	{
+		"bairro": "Montes Claros 2",
+		"quadra": "22",
+		"cep": "72906796",
+		"lat": -15.957606,
+		"lng": -48.256927,
+		"obs": ""
+	},
+	{
+		"bairro": "Montes Claros 2",
+		"quadra": "23",
+		"cep": "72906798",
+		"lat": -15.95805,
+		"lng": -48.257255,
+		"obs": ""
+	},
+	{
+		"bairro": "Montes Claros 2",
+		"quadra": "24",
+		"cep": "72906800",
+		"lat": -15.958468,
+		"lng": -48.257568,
+		"obs": "Avenida das Palmeiras"
+	},
+	{
+		"bairro": "Montes Claros 2",
+		"quadra": "25",
+		"cep": "72906806",
+		"lat": -15.958873,
+		"lng": -48.258093,
+		"obs": "Avenida das Palmeiras"
+	},
+	{
+		"bairro": "Montes Claros 2",
+		"quadra": "26",
+		"cep": "72906816",
+		"lat": -15.95925,
+		"lng": -48.258466,
+		"obs": ""
+	},
+	{
+		"bairro": "Montes Claros 2",
+		"quadra": "27",
+		"cep": "72906818",
+		"lat": -15.959719,
+		"lng": -48.258824,
+		"obs": ""
+	},
+	{
+		"bairro": "Montes Claros 2",
+		"quadra": "28",
+		"cep": "72906828",
+		"lat": -15.960078,
+		"lng": -48.259236,
+		"obs": ""
+	},
+	{
+		"bairro": "Montes Claros 2",
+		"quadra": "29",
+		"cep": "72906804",
+		"lat": -15.960606,
+		"lng": -48.257377,
+		"obs": "Avenida das Palmeiras"
+	},
+	{
+		"bairro": "Montes Claros 2",
+		"quadra": "30",
+		"cep": "72906802",
+		"lat": -15.95856,
+		"lng": -48.255907,
+		"obs": "Avenida das Palmeiras"
+	}
+	]);
