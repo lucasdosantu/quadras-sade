4723447078b3684dff20295e9777649fc78254d5
lucasdosantu <106567042+lucasdosantu@users.noreply.github.com>
2026-05-02 15:06:37 -0300

    Criar vila_esperanca.js

---
+++ js/bairros/vila_esperanca.js
@@ -0,0 +1,90 @@
+baseDeDados.push(...[
+	{
+		"bairro": "Vila Esperança",
+		"quadra": "1",
+		"cep": "72902112",
+		"lat": -15.948121,
+		"lng": -48.29208,
+		"obs": "atrás da igreja assembléia de Deus"
+	},
+	{
+		"bairro": "Vila Esperança",
+		"quadra": "2",
+		"cep": "72902110",
+		"lat": -15.947763,
+		"lng": -48.292541,
+		"obs": "3ª quadra ao lado das escolas"
+	},
+	{
+		"bairro": "Vila Esperança",
+		"quadra": "3",
+		"cep": "72902114",
+		"lat": -15.947254,
+		"lng": -48.292735,
+		"obs": "quadra do supermercado"
+	},
+	{
+		"bairro": "Vila Esperança",
+		"quadra": "4",
+		"cep": "72902116",
+		"lat": -15.946745,
+		"lng": -48.292843,
+		"obs": "ao lado das escolas"
+	},
+	{
+		"bairro": "Vila Esperança",
+		"quadra": "5",
+		"cep": "72902118",
+		"lat": -15.946222,
+		"lng": -48.293094,
+		"obs": "maria de lourdes"
+	},
+	{
+		"bairro": "Vila Esperança",
+		"quadra": "6",
+		"cep": "72902120",
+		"lat": -15.945659,
+		"lng": -48.293191,
+		"obs": "quadra ao lado das escolas"
+	},
+	{
+		"bairro": "Vila Esperança",
+		"quadra": "7",
+		"cep": "72902122",
+		"lat": -15.94515,
+		"lng": -48.293329,
+		"obs": ""
+	},
+	{
+		"bairro": "Vila Esperança",
+		"quadra": "8",
+		"cep": "72902124",
+		"lat": -15.944626,
+		"lng": -48.293598,
+		"obs": ""
+	},
+	{
+		"bairro": "Vila Esperança",
+		"quadra": "9",
+		"cep": "72902126",
+		"lat": -15.944082,
+		"lng": -48.293746,
+		"obs": ""
+	},
+	{
+		"bairro": "Vila Esperança",
+		"quadra": "10",
+		"cep": "72902128",
+		"lat": -15.943525,
+		"lng": -48.293898,
+		"obs": ""
+	},
+	{
+		"bairro": "Vila Esperança",
+		"quadra": "11",
+		"cep": "72902130",
+		"lat": -15.943036,
+		"lng": -48.293965,
+		"obs": ""
+	}
+	]);
