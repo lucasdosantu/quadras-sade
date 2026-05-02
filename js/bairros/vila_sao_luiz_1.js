422734e670802a225e8c4d3e9a59d4393a84dd27
lucasdosantu <106567042+lucasdosantu@users.noreply.github.com>
2026-05-02 15:08:03 -0300

    Criar vila_sao_luiz_1.js

---
+++ js/bairros/vila_sao_luiz_1.js
@@ -0,0 +1,74 @@
+baseDeDados.push(...[
+	{
+		"bairro": "Vila São Luiz 1",
+		"quadra": "1",
+		"cep": "72900866",
+		"lat": -15.951303,
+		"lng": -48.258396,
+		"obs": ""
+	},
+	{
+		"bairro": "Vila São Luiz 1",
+		"quadra": "2",
+		"cep": "72900864",
+		"lat": -15.951623,
+		"lng": -48.257788,
+		"obs": ""
+	},
+	{
+		"bairro": "Vila São Luiz 1",
+		"quadra": "3",
+		"cep": "72900862",
+		"lat": -15.952013,
+		"lng": -48.257277,
+		"obs": ""
+	},
+	{
+		"bairro": "Vila São Luiz 1",
+		"quadra": "4",
+		"cep": "72900860",
+		"lat": -15.952353,
+		"lng": -48.256623,
+		"obs": ""
+	},
+	{
+		"bairro": "Vila São Luiz 1",
+		"quadra": "6",
+		"cep": "72900876",
+		"lat": -15.953977,
+		"lng": -48.258137,
+		"obs": ""
+	},
+	{
+		"bairro": "Vila São Luiz 1",
+		"quadra": "5",
+		"cep": "72900872",
+		"lat": -15.953291,
+		"lng": -48.257965,
+		"obs": ""
+	},
+	{
+		"bairro": "Vila São Luiz 1",
+		"quadra": "7",
+		"cep": "72900874",
+		"lat": -15.95314,
+		"lng": -48.25965,
+		"obs": ""
+	},
+	{
+		"bairro": "Vila São Luiz 1",
+		"quadra": "8",
+		"cep": "72900870",
+		"lat": -15.952554,
+		"lng": -48.259519,
+		"obs": ""
+	},
+	{
+		"bairro": "Vila São Luiz 1",
+		"quadra": "9",
+		"cep": "782900868",
+		"lat": -15.95177,
+		"lng": -48.259375,
+		"obs": ""
+	}
+	]);
